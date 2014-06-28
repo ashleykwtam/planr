48f39e2fa491adaa1fe06f40019f0fd04730bba7
Ashley <ashleykwtam@gmail.com>
2014-06-28 14:46:28 -0400

    created a weeks controller and views

---
+++ app/controllers/weeks_controller.rb
@@ -0,0 +1,28 @@
+class WeeksController < ApplicationController
+
+  def index
+    @weeks = Week.all
+  end
+
+  def new
+    @week = Week.new
+  end
+
+  def create
+    @week = Week.new(week_params)
+    if @week.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @week = Week.find(params[:id])
+  end
+
+  private
+  def week_params
+    params.require(:week).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get "about" => 'pages#about'
 
+  resources :weeks, :except => [:destroy, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
